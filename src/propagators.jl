fed507a3675034305bf0bddd5ae4f30c5644f382
Sebastijan Dumancic <sebastijan.dumancic@gmail.com>
2021-09-27 11:13:45 +0200

    fixed containedin propagator

--- src/propagators.jl
+++ src/propagators.jl
@@ -1,16 +1,4 @@
-"""
-	Checks if elements of vec1 are contained in vec2 in the same order (possibly with elements in between)
-"""
-function containedin(vec1::Vector, vec2::Vector)
-	vec1_index = 1 # keeps track where we are in the firsst vector
-	for item in vec2
-		if item == vec1[vec1_index]
-			vec1_index += 1  # increase the index every time we encounter the matching element
-		end
-	end
 
-	vec1_index > length(vec1)
-end
 
 
 """
@@ -23,7 +11,7 @@ function propagate(c::ComesAfter, context::GrammarContext, domain::Vector{Int})
 		if containedin(c.predecessors, ancestors)
 			return domain
 		else
-			return filter!(e -> e != c.rule, domain)
+			return filter(e -> e != c.rule, domain)
 		end
 	else # if it is not in the domain, just return domain
 		return domain
